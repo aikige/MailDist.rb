b6b344a4c1df09ae47d339f0c461413a1a898e54
aikige <48190566+aikige@users.noreply.github.com>
2024-05-29 16:59:19 +0900

    [up] call setup if bundler is not ready.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=$(dirname $(realpath $0))
+BUNDLE_DIR=.bundle
+if [ -d $BUNDLE_DIR ]; then
+	exit
+fi
 if [ ! -f Gemfile ]; then
 	ln -s $SCRIPT_DIR/Gemfile
 fi
-bundle install --path .bundle
+bundle install --path $BUNDLE_DIR
