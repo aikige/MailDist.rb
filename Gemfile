7bcd95ab88872212b30661fbae6782c567d6ee05
aikige <48190566+aikige@users.noreply.github.com>
2026-02-22 10:58:03 +0900

    [up] now script works with Ruby 4.0.1.

--- Gemfile
+++ Gemfile
@@ -3,5 +3,9 @@
 source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem "mail-iso-2022-jp"
-gem "mime-types"
+gem 'nkf'
+gem 'base64'
+gem 'net-smtp'
+gem 'mail', '<= 2.7.1'
+gem 'mail-iso-2022-jp'
+gem 'mime-types'
--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 SCRIPT_DIR=$(dirname $(realpath $0))
 BUNDLE_DIR=.bundle
-if [ -d $BUNDLE_DIR ]; then
-	exit
-fi
 if [ ! -f Gemfile ]; then
 	ln -s $SCRIPT_DIR/Gemfile
 fi
-bundle install --path $BUNDLE_DIR
+bundle config set path $BUNDLE_DIR
+bundle install
